f8d6bc08ff3a34b76bc7c9972da84bc77c522252
mcevoyd <darren.mc.evoy@hotmail.co.uk>
2022-07-07 09:17:59 +0100

    Working

--- ch04_java_sql/customers/src/main/java/uk/ac/serc/customers/darren/App.java
+++ ch04_java_sql/customers/src/main/java/uk/ac/serc/customers/darren/App.java
@@ -15,13 +15,13 @@ public class App
         String database = "northwind";
         String user = "cf_readonly_login";
         String password = "BDtrp7ajxtzoMpF4WN4rGDrfIDAc0xIy";
-        String connectionUrl = "jdbc:sqlserver://" + url + ":1433;databaseName=" 
-        + database + ";user="+ user + ";password=" + password +";";
-
-        ArrayList <String> customers = new ArrayList<String>();
+        String connectionUrl = "jdbc:sqlserver://" + url + ":1433;databaseName=" + database + ";user="+ user + ";password=" + password +";";
     
         System.out.println(connectionUrl);
 
+        // creating an array list for the customers
+        ArrayList<Customer> customerList = new ArrayList<Customer>();
+        
         try {
             // Load SQL Server JDBC driver and establish connection.
             System.out.print("Connecting to SQL Server ... ");
@@ -32,25 +32,32 @@ public class App
                 try (Statement statement = connection.createStatement();
                         ResultSet resultSet = statement.executeQuery(sql)) {
                     while (resultSet.next()) {
+                        //adding to the array list using the Customer constructor  
+                        customerList.add(new Customer(resultSet.getString("CustomerID"), 
+                                resultSet.getString("CompanyName"),
+                                resultSet.getString("ContactName"),
+                                resultSet.getString("ContactTitle"),
+                                resultSet.getString("Address"),
+                                resultSet.getString("City"),
+                                resultSet.getString("Region"),
+                                resultSet.getString("PostalCode"),
+                                resultSet.getString("Country"),
+                                resultSet.getString("Phone"),
+                                resultSet.getString("Fax")
+                                ));
 
-                        // customers.add(new Customer(resultSet.getString("CustomerID",
-                        // resultSet.getString("CompanyName"),
-                        // resultSet.getString("ContactName")));
-
-                        customers.add(resultSet.getString("CustomerID") 
-                        + " | " + resultSet.getString("CompanyName") + " | " 
-                        + resultSet.getString("ContactName"));
-
-
-                        // System.out.println(resultSet.getString("CustomerID") 
-                        // + " | " + resultSet.getString("CompanyName") + " | " 
-                        // + resultSet.getString("ContactName"));
-
-                        for (int x =0; x < customers.size(); x++){
-                            System.out.print(customers);
-                            System.out.println("");
-                        }
+                        
+                        /* Brian's original code for printing out the customers' details
+                        System.out.println(resultSet.getString("CustomerID") + " | " + resultSet.getString("CompanyName") + " | " + resultSet.getString("ContactName"));
+                        */
                     }
+
+                    // print out the array list
+                    for(Customer customer : customerList){
+                        System.out.println(customer.toString());
+                        System.out.println(" ");
+                    }
+                    
                 }
                 connection.close();
                 System.out.println("All done.");
@@ -59,6 +66,5 @@ public class App
             System.out.println();
             e.printStackTrace();
         }
-
     }
 }
--- ch04_java_sql/customers/src/main/java/uk/ac/serc/customers/darren/Customer.java
+++ ch04_java_sql/customers/src/main/java/uk/ac/serc/customers/darren/Customer.java
@@ -2,12 +2,94 @@ package uk.ac.serc.customers.darren;
 
 public class Customer {
 
-    String CustomerID, CompanyName, ContactName;
+    String CustomerID, CompanyName, ContactName, contactTitle, address, city, region, postalCode, country, phone, fax;
 
-    public Customer(String customerID, String companyName, String contactName) {
+   
+
+    @Override
+    public String toString() {
+        return "Customer [CompanyName=" + CompanyName + ", ContactName=" + ContactName + ", CustomerID=" + CustomerID
+                + ", address=" + address + ", city=" + city + ", contactTitle=" + contactTitle + ", country=" + country
+                + ", fax=" + fax + ", phone=" + phone + ", postalCode=" + postalCode + ", region=" + region + "]";
+    }
+
+    public Customer(String customerID, String companyName, String contactName, String contactTitle, String address,
+            String city, String region, String postalCode, String country, String phone, String fax) {
         CustomerID = customerID;
         CompanyName = companyName;
         ContactName = contactName;
+        this.contactTitle = contactTitle;
+        this.address = address;
+        this.city = city;
+        this.region = region;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.phone = phone;
+        this.fax = fax;
+    }
+
+    public String getContactTitle() {
+        return contactTitle;
+    }
+
+    public void setContactTitle(String contactTitle) {
+        this.contactTitle = contactTitle;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
     }
 
     public String getCustomerID() {
@@ -34,11 +116,7 @@ public class Customer {
         ContactName = contactName;
     }
 
-    @Override
-    public String toString() {
-        return "Customer [CompanyName=" + CompanyName + ", ContactName=" + ContactName + ", CustomerID=" + CustomerID
-                + "]";
-    }
+    
 
     
 }
